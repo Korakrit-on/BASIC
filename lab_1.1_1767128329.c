0976b2fb0cffb84f041beb6fcdc8680c3e4ef59d
Korakrit Borisuth <captainza.055@gmail.com>
2025-12-31 03:58:50 +0700

    Code 1.1

---
+++ lab_1.1_1767128329.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    
+    // รับค่า N
+    if (scanf("%d", &N) != 1) {
+        return 1; 
+    }
+    
+    // ตรวจสอบเงื่อนไข N <= 0 (ตาม Test Case 3 และ 4)
+    if (N <= 0) {
+        printf("Error\n");
+    } else {
+        // วนซ้ำเพื่อแสดงผล "Hello Loop!" N ครั้ง
+        for (i = 0; i < N; i++) {
+            printf("Hello Loop!\n");
+        }
+    }
+
+    return 0;
+}

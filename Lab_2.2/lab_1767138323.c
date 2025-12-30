39a1ed68dcbcf9d86eaeaadc9373abdc0fa17663
Korakrit Borisuth <captainza.055@gmail.com>
2025-12-31 06:45:23 +0700

    msg

---
+++ Lab_2.2/lab_1767138323.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("เว็บไซต์กำลังอยู่ในระหว่างปรับปรุง แล้วเราจะได้พบกันใหม่เร็วๆ นี้\n");
+    printf("– admin\n");
+    printf("ติดตามเรา\n");
+    printf("Copyright \251 2025 Thailand Fx Warrior\n");
+    return 0;
+}

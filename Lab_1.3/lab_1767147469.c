7b5d6aae9d3271f2273c8cc48de4d09dfc583cd4
Korakrit Borisuth <captainza.055@gmail.com>
2025-12-31 09:17:50 +0700

    msg

---
+++ Lab_1.3/lab_1767147469.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, operationCode, result;
+    
+    if (scanf("%d %d %d", &num1, &num2, &operationCode) != 3) {
+        return 1; 
+    }
+    
+    switch (operationCode) {
+        case 1:
+            result = num1 + num2;
+            printf("%d\n", result);
+            break;
+        case 2:
+            result = num1 - num2;
+            printf("%d\n", result);
+            break;
+        case 3:
+            result = num1 * num2;
+            printf("%d\n", result);
+            break;
+        case 4:
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%d\n", result);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        default:
+            printf("Invalid Operation\n");
+            break;
+    }
+    
+    return 0;
+}
